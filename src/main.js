42e4ceaecb1c14903cf86ab9b1094d2b2df2d40b
venuguggillavv <31430458+venuguggillavv@users.noreply.github.com>
2024-02-01 23:55:49 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/YZj2Klf4lZ-KjHwH/scene.splinecode');
